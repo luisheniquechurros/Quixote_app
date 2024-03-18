b22f8430346a07bd51acb449b4436b6ab1e7ceab
luisheniquechurros <churros9614@gmail.com>
2024-03-18 11:26:14 -0300

    Atualizações na tela de login
    Adicionei os campos de escrever, algumas imagens svg e um botão

--- flutter/lib/pages/login_page.dart
+++ flutter/lib/pages/login_page.dart
@@ -12,13 +12,38 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 80),
+            padding: const EdgeInsets.only(top: 18, left: 0),
             child: SvgPicture.asset(
-              'assets/images/decerto.svg',
-              width: 322,
-              height: 124,
+              'assets/images/arcoiris.svg',
+              width: 333,
+              height: 152,
             ),
           ),
+          Row(children: [Padding
+          (padding: EdgeInsets.only(top: 179, left:131),
+          child: Text('Ao se inscrever você está ',
+           style: TextStyle(fontSize: 12),
+           textAlign: TextAlign.justify),)
+           ],
+           ),
+           Row(children: [Padding
+          (padding: EdgeInsets.only(top: 189, left:112 ),
+          child: Text('concordando em quebrar as regras  ',
+           style: TextStyle(fontSize: 12),
+           textAlign: TextAlign.justify),)
+           ],
+           ),
+           Container(
+            padding: EdgeInsets.only(left: 24, right: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(decoration: InputDecoration(hintText: 'Endereço de e-mail')),
+                TextField(decoration: InputDecoration(hintText: 'Senha'), obscureText: true,),
+                ElevatedButton(onPressed:() {}, child: Text('Login')),
+              ],
+            ),
+           )
         ],
       ),
     );
