75c6c379c1f4abe0bac753b893d4fc838687f2b4
luisheniquechurros <131274765+luisheniquechurros@users.noreply.github.com>
2024-03-18 06:56:05 -0300

    imagem

--- flutter/lib/pages/login_page.dart
+++ flutter/lib/pages/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}): super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -11,16 +11,16 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
-          Padding(padding: const EdgeInsets.only(top: 80, right: 38),
-          child: SvgPicture.asset(
-            'assets/images/A.svg',
-            width: 500,
-            height: 1090,
-          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: SvgPicture.asset(
+              'assets/images/decerto.svg',
+              width: 322,
+              height: 124,
+            ),
           ),
         ],
       ),
     );
   }
 }
-
