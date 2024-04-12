06e415e0d42ab2a9a8889d98dcd9b9300cf4bf76
luisheniquechurros <churros9614@gmail.com>
2024-04-12 11:29:18 -0300

    login page

--- flutter/lib/pages/login_page.dart
+++ flutter/lib/pages/login_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // Variáveis de controle
+  bool isPassowordVisible = false; // Variável para controlar a visibilidade da senha
+  bool isLoading = false; // Variável para indicar se o aplicativo está carregando
+  String versaoApp = ''; // Variável para armazenar a versão do aplicativo
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -11,6 +22,7 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
+          // Imagem da mancha do entre
           Padding(
             padding: const EdgeInsets.only(top: 18, left: 0),
             child: SvgPicture.asset(
@@ -19,31 +31,79 @@ class LoginPage extends StatelessWidget {
               height: 152,
             ),
           ),
-          Row(children: [Padding
-          (padding: EdgeInsets.only(top: 179, left:131),
-          child: Text('Ao se inscrever você está ',
-           style: TextStyle(fontSize: 12),
-           textAlign: TextAlign.justify),)
-           ],
-           ),
-           Row(children: [Padding
-          (padding: EdgeInsets.only(top: 189, left:112 ),
-          child: Text('concordando em quebrar as regras  ',
-           style: TextStyle(fontSize: 12),
-           textAlign: TextAlign.justify),)
-           ],
-           ),
-           Container(
+          // Texto 1
+          Row(children: [
+            Padding(
+              padding: EdgeInsets.only(top: 179, left: 116),
+              child: Text(
+                'Ao se inscrever você está ',
+                style: TextStyle(fontSize: 15),
+                textAlign: TextAlign.justify,
+              ),
+            )
+          ]),
+          // Texto 1
+          Row(children: [
+            Padding(
+              padding: EdgeInsets.only(top: 195, left: 103),
+              child: Text(
+                'concordando em quebrar as regras  ',
+                style: TextStyle(fontSize: 15),
+                textAlign: TextAlign.justify,
+              ),
+            )
+          ]),
+          // Formulário de login
+          Container(
             padding: EdgeInsets.only(left: 24, right: 24),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextField(decoration: InputDecoration(hintText: 'Endereço de e-mail')),
-                TextField(decoration: InputDecoration(hintText: 'Senha'), obscureText: true,),
-                ElevatedButton(onPressed:() {}, child: Text('Login')),
+                // Campo de e-mail
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: const TextStyle(fontSize: 16),
+                  decoration: const InputDecoration(
+                    hintText: "Endereço de Email",
+                    hintStyle: TextStyle(fontSize: 15),
+                  ),
+                ),
+                // Campo de senha
+                TextFormField(
+                  maxLength: 12,
+                  obscureText: !isPassowordVisible, // Usando a variável para controlar a visibilidade da senha
+                  style: const TextStyle(fontSize: 16),
+                  decoration: InputDecoration(
+                    hintText: "Senha",
+                    suffixIcon: IconButton(
+                      icon: isPassowordVisible
+                          ? const Icon(Icons.visibility)
+                          : const Icon(Icons.visibility_off),
+                      onPressed: () {
+                        setState(() {
+                          isPassowordVisible = !isPassowordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                // Botão de login
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(318, 45),
+                    textStyle: TextStyle(fontSize: 17),
+                    backgroundColor: const Color(0xFF0386D0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    foregroundColor: Colors.white,
+                  ),
+                  child: Text('Login'),
+                ),
               ],
             ),
-           )
+          )
         ],
       ),
     );
